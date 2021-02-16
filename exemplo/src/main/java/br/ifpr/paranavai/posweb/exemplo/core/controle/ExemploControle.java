7c0b34b712eb8b9ed4c8014244c78295d34c5659
Jaguanhara <51683220+Jaguanhara@users.noreply.github.com>
2021-02-16 11:08:49 -0300

    Exemplo Aula

---
+++ exemplo/src/main/java/br/ifpr/paranavai/posweb/exemplo/core/controle/ExemploControle.java
@@ -0,0 +1,21 @@
+package br.ifpr.paranavai.posweb.exemplo.core.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExemploControle {
+
+	@RequestMapping("/dados")
+	
+	public String getDados(Model model){
+		
+		model.addAttribute("mensagem","Hello World!");
+		
+		return "dados/index";
+		
+	}
+			
+		
+}
